fc8b606e7763fe88eed3cf45711e49158c2e601c
paras verma <parasverma2531@gmail.com>
2023-10-17 14:01:50 +0530

    Login,Signup module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,59 +1,62 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { axiosClient } from "../../utils/axiosClient";
-import {
-  KEY_ACCESS_TOKEN,
-  setItem,
-} from "../../utils/localStorageManager";
-import "./Login.scss";
-function Login() {
+import "./Signup.scss";
+function Signup() {
   //fetch the data::
   const [userEmail, setUserEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  const navigate = useNavigate();
   //function to handle Data on submit
   async function handleSubmit(event) {
     event.preventDefault(); //prevent the default behaviour of form
     try {
       const result = await axiosClient.post(
-        "/auth/login",
+        "/auth/signup",
         //send the data in the body of API
         {
           email: userEmail,
+          name: userName,
           password: userPassword,
         }
       );
-      setItem(KEY_ACCESS_TOKEN, result.response.accessToken); //save the access token to localStorage
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    <div className="Login">
-      <div className="login-box">
-        <h2 className="login-heading">Login</h2>
+    <div className="Signup">
+      <div className="signup-box">
+        <h2 className="signup-heading">Signup</h2>
         <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            className="signup-input"
+            onChange={(event) => setUserName(event.target.value)}
+          />
           <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
-            className="login-input"
+            className="signup-input"
             onChange={(event) => setUserEmail(event.target.value)}
           />
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            className="login-input"
+            className="signup-input"
             onChange={(event) => setUserPassword(event.target.value)}
           />
-          <input type="submit" className="login-submit" />
+          <input type="submit" className="signup-submit" />
         </form>
-        <p className="signup-Navigate">
-          Create an Account?
+        <p className="login-Navigate">
+          Already have an Account?
           <span>
-            <Link className="link" to="/signup">
-              Signup
+            <Link className="link" to="/login">
+              Login
             </Link>
           </span>
         </p>
@@ -61,4 +64,5 @@ function Login() {
     </div>
   );
 }
-export default Login;
+
+export default Signup;
